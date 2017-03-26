847e9c280cc7146cfd99aa6194eecbfb7ba920e9
Sijan <sijanonly@gmail.com>
2017-03-26 14:59:43 +0545

    home page is designed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# APPS_DIR = BASE_DIR.path('portfolio')
+# print('app', APPS_DIR)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "portfolio", "static"),
+)
--- sijanprofile/views.py
+++ sijanprofile/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+"""Views for the base app"""
+
+from django.shortcuts import render
+
+
+def home(request):
+    """ Default view for the root """
+    return render(request, 'index.html')
