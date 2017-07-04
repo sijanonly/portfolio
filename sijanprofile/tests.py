65b27b7477efeba0058ab0f5c8953e22fca5d01b
sijantech <sijanbhandari@lftechnology.com>
2017-07-04 18:25:54 +0545

    Integrating circleCI

--- sijanprofile/tests.py
+++ sijanprofile/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+from sijanprofile.views import TestMath
+
+
+class MathTest(TestCase):
+    def test_addition(self):
+        # Make test fail
+        self.assertEqual(TestMath.add_numbers(3, 4), 8)
--- sijanprofile/views.py
+++ sijanprofile/views.py
@@ -11,3 +11,9 @@ from django.shortcuts import render
 def home(request):
     """ Default view for the root """
     return render(request, 'index.html')
+
+
+class TestMath():
+
+    def add_numbers(num1, num2):
+        return num1 + num2
